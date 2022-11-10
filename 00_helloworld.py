3804fc02d4ff38144ff158dec298c5a251cf0e81
Pinsho <pinsho@gmail.com>
2022-11-10 14:01:38 +0100

    Tipos de datos

---
+++ 00_helloworld.py
@@ -0,0 +1,20 @@
+# Comentarios
+# Hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Comentario en
+varias líneas
+"""
+
+'''
+Más comentarios
+en varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(3)) # Tipo 'int'
+print(type(2.5)) # Tipo 'float'
+print(type(1 + 2j)) # Tipo 'complex'
